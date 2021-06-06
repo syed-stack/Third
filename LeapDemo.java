29976fbd63b7cf72cdbc21f9a2283c66b57720f1
syed-stack <84707016+syed-stack@users.noreply.github.com>
2021-06-06 09:41:01 +0530

    16

---
+++ LeapDemo.java
@@ -0,0 +1,33 @@
+package sixteenth;
+import java.util.Scanner;
+
+public class LeapDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter any year : ");
+		year = scan.nextInt();
+		scan.close();
+		boolean isLeap = false;
+		if(year % 4 ==0)
+		{
+			if(year %100 ==0)
+			{
+				if(year % 400 ==0)
+					isLeap = true;
+				else
+					isLeap = false;
+			}
+			else isLeap = false;
+		}
+		if(isLeap == true)
+			System.out.println(year + "is a leap year");
+		else
+			System.out.println(year + "is not a leap year");
+
+
+	}
+
+}

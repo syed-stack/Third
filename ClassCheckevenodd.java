18a03eeed8a853cb3435b763b1df807566ef3139
syed-stack <84707016+syed-stack@users.noreply.github.com>
2021-06-06 08:15:14 +0530

    Fifth

---
+++ ClassCheckevenodd.java
@@ -0,0 +1,16 @@
+package fifth;
+
+public class ClassCheckevenodd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number =16;
+		if (number % 2 ==0)
+			System.out.println("given number is an even");
+		else
+			System.out.println("given number is an odd");
+
+
+	}
+
+}

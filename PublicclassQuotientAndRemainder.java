5eb27398a2b4a6997e19f920df0c4c6eece00f19
syed-stack <84707016+syed-stack@users.noreply.github.com>
2021-06-06 09:36:46 +0530

    2

---
+++ PublicclassQuotientAndRemainder.java
@@ -0,0 +1,13 @@
+package Second;
+
+public class PublicclassQuotientAndRemainder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 150, y = 25;
+		System.out.println("quotient of two numbers :"+ (x/y));
+		System.out.println("remainder of two numbers is :"+ (x%y));
+
+	}
+
+}
